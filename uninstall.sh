e5d5a834b0bc4c998da289094156692c1ea65af8
Matías Blasi <matias@matiasblasi.com>
2020-04-25 15:53:35 -0300

    Install & Uninstall scripts

---
+++ uninstall.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo "Removing FletaloYa scripts from /usr/local/bin"
+
+rm /usr/local/bin/fyauth /usr/local/bin/fylogin /usr/local/bin/fyme /usr/local/bin/fyoffers /usr/local/bin/fyrefresh /usr/local/bin/fyrequests
+
+echo "For installing again just run './install.sh'"
