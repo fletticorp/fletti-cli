11db42906e6fad9de4cc3f4f965da0f2a5ce04ee
Matías Blasi <matias@matiasblasi.com>
2020-04-25 16:55:19 -0300

    Corrijo comillas

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-echo "Installing FletaloYa scripts into `/usr/local/bin`"
+echo "Installing FletaloYa scripts into '/usr/local/bin'"
 
 cp scripts/fy* /usr/local/bin
 
-echo "For uninstalling just run `./uninstall.sh`"
+echo "For uninstalling just run './uninstall.sh'"
--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-echo "Removing FletaloYa scripts from `/usr/local/bin`"
+echo "Removing FletaloYa scripts from '/usr/local/bin'"
 
 rm /usr/local/bin/fyauth /usr/local/bin/fylogin /usr/local/bin/fyme /usr/local/bin/fyoffers /usr/local/bin/fyrefresh /usr/local/bin/fyrequests
 
-echo "For installing again just run `./install.sh`"
+echo "For installing again just run './install.sh'"
